6ed864eed1c76c3e9ad4b064140ab47c54d18687
Andrew Vyakhirev <andrewvyakhirev@Andrews-MacBook-Pro.local>
2020-03-17 11:48:24 +0200

    added feeAmount functional

--- AtomicSwap.sol
+++ AtomicSwap.sol
@@ -1,6 +1,78 @@
 pragma solidity 0.6.4;
 
-contract AtomicSwap {
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+    address public owner;
+    
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+    * account.
+    */
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    /**
+    * @dev Throws if called by any account other than the owner.
+    */
+    modifier onlyOwner() {
+        require(msg.sender == owner, "msg.sender's address isn't owner address");
+        _;
+    }
+
+    /**
+    * @dev Allows the current owner to transfer control of the contract to a newOwner.
+    * @param newOwner The address to transfer ownership to.
+    */
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
+
+contract AtomicSwap is Ownable {
+    
+    using SafeMath for uint256;
 
     struct Swap {
         bytes32 hashedSecret;
@@ -11,32 +83,36 @@ contract AtomicSwap {
         uint nonce;
         address payable participant;
         uint256 value;
+        uint256 fee;
         bool emptied;
         bool initiated;
     }
 
     mapping(bytes32 => Swap) public swaps;
+    
+    uint8 public fee;
+    uint256 public feeAmount;
+    
+    constructor() public {
+        fee = 5;
+        feeAmount = 0;
+    }
 
-    event Refunded(
-        bytes32 indexed _hashedSecret,
-        uint _refundTime
-    );
-    event Redeemed(
-        bytes32 indexed _hashedSecret,
-        bytes32 _secret,
-        uint _redeemTime
-    );
+    event Refunded(bytes32 indexed _hashedSecret, uint _refundTime);
+    
+    event Redeemed(bytes32 indexed _hashedSecret, bytes32 _secret, uint _redeemTime);
+    
     event Initiated(
         bytes32 indexed _hashedSecret,
         uint _initTimestamp,
         uint _refundTime,
         address indexed _participant,
         address indexed _initiator,
-        uint256 _value
+        uint256 _value,
+        uint256 _fee
     );
-
-    constructor() public {
-    }
+    
+    event WithdrawFee(address indexed _owner, uint256 amount);
 
     modifier isRefundable(bytes32 _hashedSecret) {
         require(block.timestamp > swaps[_hashedSecret].initTimestamp + swaps[_hashedSecret].refundTime, "refundTime has not come");
@@ -70,6 +146,7 @@ contract AtomicSwap {
         swaps[_hashedSecret].initiator = msg.sender;
         swaps[_hashedSecret].participant = _participant;
         swaps[_hashedSecret].value = msg.value;
+        swaps[_hashedSecret].fee = msg.value.div(100).mul(fee);
         swaps[_hashedSecret].initiated = true;
 
         emit Initiated(
@@ -78,33 +155,32 @@ contract AtomicSwap {
             swaps[_hashedSecret].refundTime,
             swaps[_hashedSecret].participant,
             msg.sender,
-            swaps[_hashedSecret].value
+            swaps[_hashedSecret].value,
+            swaps[_hashedSecret].fee
         );
     }
 
     function redeem(bytes32 _hashedSecret, bytes32 _secret) public isInitiated(_hashedSecret) isRedeemable(_hashedSecret, _secret) {
         swaps[_hashedSecret].emptied = true;
         swaps[_hashedSecret].secret = _secret;
-
-        emit Redeemed(
-            _hashedSecret,
-            _secret,
-            block.timestamp
-        );
-
-        swaps[_hashedSecret].participant.transfer(swaps[_hashedSecret].value);
+        emit Redeemed(_hashedSecret, _secret, block.timestamp);
+        swaps[_hashedSecret].participant.transfer(swaps[_hashedSecret].value-swaps[_hashedSecret].fee);
+        feeAmount = feeAmount.add(swaps[_hashedSecret].fee);
     }
 
     function refund(bytes32 _hashedSecret) public isInitiated(_hashedSecret) isRefundable(_hashedSecret) {
         swaps[_hashedSecret].emptied = true;
         swaps[_hashedSecret].initiated = false;
-
-        emit Refunded(
-            _hashedSecret,
-            block.timestamp
-        );
-
+        emit Refunded(_hashedSecret, block.timestamp);
         swaps[_hashedSecret].initiator.transfer(swaps[_hashedSecret].value);
+    }
+    
+    function withdrawFee(uint256 _amount) public payable onlyOwner returns (bool) {
+        require(_amount <= feeAmount, "amount more than feeAmount");
+        feeAmount = feeAmount.sub(_amount);
+        msg.sender.transfer(_amount);
+        emit WithdrawFee(msg.sender, _amount);
+        return true;
     }
     
     function stringToBytes32(string memory source) public pure returns (bytes32 result) {
@@ -120,25 +196,9 @@ contract AtomicSwap {
     function bytes32ToSHA256(bytes32 _secret) public pure returns(bytes32) {
         return sha256(abi.encodePacked(_secret));
     }
- 
-    function bytes32To2SHA256(bytes32 _secret) public pure returns(bytes32) {
-        return sha256(abi.encodePacked(sha256(abi.encodePacked(_secret))));
-    }
-    
-    function bytes32ToRipemd160 (bytes32 _secret) public pure returns(bytes20) {
-        return ripemd160(abi.encodePacked(_secret));
-    }
-    
-    function stringToRipemd160 (string memory _secret) public pure returns(bytes20) {
-        return ripemd160(abi.encodePacked(_secret));
-    }
     
     function stringToSHA256(string memory source) public pure returns(bytes32) {
         return sha256(abi.encodePacked(source));
-    }
-    
-    function stringTo2SHA256(string memory source) public pure returns(bytes32) {
-        return sha256(abi.encodePacked(sha256(abi.encodePacked(source))));
     }
     
     function getTimestamp() public view returns(uint256) {
